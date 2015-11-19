f31c66b100722275c8dd448167a4e2920e48dfaf
rallen_temp <richard.alexander.allen@example.com>
2015-11-19 17:49:33 +0000

    Initial hook_menu() implementation.
    This is a prototype - work in progress.

--- gt_feeds/gt_feeds.module.php
+++ gt_feeds/gt_feeds.module.php
@@ -18,3 +18,19 @@ function gt_feeds_permission() {
   return $items;
 }
 
+
+/**
+ * Implements hook_menu().
+ */
+function gt_feeds_menu() {
+  $items = array();
+  # /feed/<content_type>/<taxonomy_vocabulary_name>/<term_names>
+  $items['feed/<content_type>/<taxonomy_vocabulary_name>/<term_names>'] = array(
+    'page callback' => 'gt_feeds_xml_print',
+    'access arguments' => array('access greatist feeds'),
+    'page arguments' => array(1, '%gt_feeds_content_type', '%gt_feeds_vocabulary', '%gt_feeds_term'), # %mymodule_abc -> mymodule_abc_load()
+    'type' => MENU_CALLBACK,
+  );
+  return $items;
+}
+
