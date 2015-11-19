7c1f2686aeb07cce65b7cb636b27a6a5f841f879
rallen_temp <richard.alexander.allen@example.com>
2015-11-19 17:48:11 +0000

    Initial hook_permission() implementation.

--- gt_feeds/gt_feeds.module.php
+++ gt_feeds/gt_feeds.module.php
@@ -6,3 +6,15 @@
  * Provides XML feeds.
  */
 
+/**
+ * Implements hook_permission().
+ */
+function gt_feeds_permission() {
+  $items = array();
+  $items['access greatist feeds'] = array(
+    'title' => t('Access Greatist Feeds'),
+    'description' => t('Allows access to XML feeds.'),
+  );
+  return $items;
+}
+
